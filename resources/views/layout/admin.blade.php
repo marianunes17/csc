082dd47ce1a8cec089464cf2fbe21a80f419b3c0
marianunes17 <mariajoao.99@hotmail.com>
2019-11-30 01:44:32 +0000

    Update admin.blade.php

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -17,49 +17,36 @@
         <!-- MAIN CSS -->
         <link rel="stylesheet" href="{{asset('css/main.css')}}">
         <!-- FOR DEMO PURPOSES ONLY. You should remove this in your project -->
-        <link rel="stylesheet" href="css/demo.css">
+        <link rel="stylesheet" href="{{asset('css/demo.css')}}">
         <!-- GOOGLE FONTS -->
         <link
             href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700"
             rel="stylesheet">
         <!-- ICONS -->
-        <link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon.png">
+        <link rel="apple-touch-icon" sizes="76x76"
+            href="{{asset('img/favicon.ico')}}">
+
         <link rel="icon" type="image/png" sizes="96x96"
-            href="{{asset('img/favicon.png')}}">
+            href="{{asset('img/favicon.ico')}}">
     </head>
 
     <body>
-        <!-- WRAPPER -->
         <div id="wrapper">
 
-            <!-- LEFT SIDEBAR -->
             @include ('layout.partials.admin_nav')
-            <!-- END LEFT SIDEBAR -->
-            <!-- MAIN -->
+
             <div class="main">
-                <!-- MAIN CONTENT -->
-
                 <div class="container-fluid">
+                    <!--Mensagens de erros-->
 
-                    <!-- End of Topbar -->
-                    @if ($errors->any())
-                    @include ('partials.errors')
-                    @endif
-                    @if (!empty(session('success')))
-                    @include ('partials.success')
-                    @endif
-                    <!-- Begin Page Content -->
+
+
                     @yield("content")
-
-
                 </div>
-                <!-- END MAIN CONTENT -->
-
-
             </div>
-            <!-- END MAIN -->
+
             @include ('layout.partials.admin_footer')
 
-        </body>
-
-    </html>
+        </div>
+    </body>
+</html>
