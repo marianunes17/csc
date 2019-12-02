6665ad230bf2e175c326ecf8bda4780ed16ba78e
CarolinaLopes99 <2180479@my.ipleiria.pt>
2019-12-02 16:22:24 +0000

    add logo-simbolo2.png, update nav.blade.php

--- resources/views/layout/partials/nav.blade.php
+++ resources/views/layout/partials/nav.blade.php
@@ -1,6 +1,6 @@
 <div id="navbar" role="navigation" class="navbar navbar-expand-lg">
     <div class="container"><a href="{{route('csc.index')}}" class="navbar-brand home">
-            <img src="img/logos/logo-simbolo.png" alt="Centro Social de
+            <img src="img/logos/logo-simbolo2.png" alt="Centro Social de
                 Carnide logo"
                 class="d-none d-md-inline-block logo"> <span
                 class="site-description">Centro Social de Carnide</span>
@@ -77,7 +77,7 @@
                 </li>
             </ul>
         </div>
-        
+
         <div id="search" class="collapse clearfix">
             <form role="search" class="navbar-form">
                 <div class="input-group">
