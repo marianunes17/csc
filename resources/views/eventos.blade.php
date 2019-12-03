3ce80ca900417eeca9b92f4d4e751a7792b05238
marianunes17 <mariajoao.99@hotmail.com>
2019-12-03 12:36:51 +0000

    Update eventos.blade.php

--- resources/views/eventos.blade.php
+++ resources/views/eventos.blade.php
@@ -6,41 +6,316 @@
 
 @include ('layout.partials.banner')
 <div class="container">
-<div class="row">
-    <div class="col-lg-3">
-        <div class="panel panel-default sidebar-menu with-icons">
-            <div class="panel-heading">
-                <h3 class="h4 panel-title">Eventos</h3>
-            </div>
+    <div class="row">
+        <div class="col-lg-3">
+            <div class="panel panel-default sidebar-menu with-icons">
+                <div class="panel-heading">
+                    <h3 class="h4 panel-title">Categorias</h3>
+                </div>
 
-            <div class="panel-body">
-                <ul class="nav nav-pills flex-column text-sm">
-                    <li class="nav-item"><a href="#" class="nav-link active">Atividades</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">Projetos</a></li>
-                </ul>
+                <div class="panel-body">
+                    <ul class="nav nav-pills flex-column text-sm">
+                        <li class="nav-item"><a href="#" class="nav-link
+                                active">Atividades</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Projetos</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
 
-    <div class="col-lg-9">
-        <div class="card text-center">
-            <div class="card-header">
-                Dia do pijama
-            </div>
+        <div class="col-lg-9">
+            <div class="card ">
+                <div class="card-header text-center">
+                        PICNIC NO PARQUE
+                </div>
 
-            <div class="card-body">
-                <p class="card-text">With supporting text below as a natural
-                    lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
+                <div class="card-body">
+                    <p class="card-text">
+                            Hoje reunimos crianças e graúdos e fizemos um picnic no parque.
+                        </p>
 
-            <div class="card-footer text-muted">
-                2 days ago
+                    <p class="card-text">
+                        Juntámos o ar puro da natureza com muita comida saudável, imensas atividades e muita diversão
+                    </p>
+                </div>
+
+
+                <!--Imagens-->
+                <div class="row">
+                        <!-- Imagem 1 -->
+                        <div class="col-lg-4 col-md-12 mb-4">
+
+                            <!--Modal: Name-->
+                            <div class="modal fade" id="modal1" tabindex="-1" role="dialog"
+                                aria-labelledby="myModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-lg" role="document">
+
+                                    <!--Content-->
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <p> asdhfgntjl,dewkmrfnasdhfgntjl,dewkmrfnjdmjdm </p>
+                                            <button type="button" class="btn btn-outline-primary
+                                                btn-rounded btn-md ml-4" data-dismiss="modal"
+                                                aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+
+                                        <!--Body-->
+                                        <div class="modal-body mb-0 p-0">
+
+                                            <div class="embed-responsive">
+                                                <img class="img-fluid z-depth-1"
+                                                    src="{{route('img/atividade1_1.jpg')}}"
+                                                    alt="video"
+                                                    data-toggle="modal" data-target="#modal1">
+                                            </div>
+
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <!--Modal: Name-->
+
+                            <a><img class="img-fluid z-depth-1"
+                                    src="https://mdbootstrap.com/img/screens/yt/screen-video-1.jpg"
+                                    alt="video"
+                                    data-toggle="modal" data-target="#modal1"></a>
+
+                        </div>
+
+                        <!--Imagem 2-->
+                        <div class="col-lg-4 col-md-12 mb-4">
+
+                            <!--Modal: Name-->
+                            <div class="modal fade" id="modal1" tabindex="-1" role="dialog"
+                                aria-labelledby="myModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-lg" role="document">
+
+                                    <!--Content-->
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <p> asdhfgntjl,dewkmrfnjdm </p>
+                                            <button type="button" class="btn btn-outline-primary
+                                            btn-rounded btn-md ml-4" data-dismiss="modal"
+                                            aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                        </div>
+                                        <!--Body-->
+                                        <div class="modal-body mb-0 p-0">
+
+                                            <div class="embed-responsive">
+                                                <img class="img-fluid z-depth-1"
+                                                    src="https://mdbootstrap.com/img/screens/yt/screen-video-1.jpg"
+                                                    alt="video"
+                                                    data-toggle="modal" data-target="#modal1">
+                                            </div>
+
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <!--Modal: Name-->
+
+                            <a><img class="img-fluid z-depth-1"
+                                    src="https://mdbootstrap.com/img/screens/yt/screen-video-1.jpg"
+                                    alt="video"
+                                    data-toggle="modal" data-target="#modal1"></a>
+
+                        </div>
+                         <!--Imagem 2-->
+                         <div class="col-lg-4 col-md-12 mb-4">
+
+                                <!--Modal: Name-->
+                                <div class="modal fade" id="modal1" tabindex="-1" role="dialog"
+                                    aria-labelledby="myModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-lg" role="document">
+
+                                        <!--Content-->
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <p> asdhfgntjl,dewkmrfnjdm </p>
+                                                <button type="button" class="btn btn-outline-primary
+                                                btn-rounded btn-md ml-4" data-dismiss="modal"
+                                                aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                            </div>
+                                            <!--Body-->
+                                            <div class="modal-body mb-0 p-0">
+
+                                                <div class="embed-responsive">
+                                                    <img class="img-fluid z-depth-1"
+                                                        src="https://mdbootstrap.com/img/screens/yt/screen-video-1.jpg"
+                                                        alt="video"
+                                                        data-toggle="modal" data-target="#modal1">
+                                                </div>
+
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <!--Modal: Name-->
+
+                                <a><img class="img-fluid z-depth-1"
+                                        src="https://mdbootstrap.com/img/screens/yt/screen-video-1.jpg"
+                                        alt="video"
+                                        data-toggle="modal" data-target="#modal1"></a>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="row">
+                            <!-- Imagem 1 -->
+                            <div class="col-lg-4 col-md-12 mb-4">
+
+                                <!--Modal: Name-->
+                                <div class="modal fade" id="modal1" tabindex="-1" role="dialog"
+                                    aria-labelledby="myModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-lg" role="document">
+
+                                        <!--Content-->
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <p> asdhfgntjl,dewkmrfnasdhfgntjl,dewkmrfnjdmjdm </p>
+                                                <button type="button" class="btn btn-outline-primary
+                                                    btn-rounded btn-md ml-4" data-dismiss="modal"
+                                                    aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+
+                                            <!--Body-->
+                                            <div class="modal-body mb-0 p-0">
+
+                                                <div class="embed-responsive">
+                                                    <img class="img-fluid z-depth-1"
+                                                        src="https://mdbootstrap.com/img/screens/yt/screen-video-1.jpg"
+                                                        alt="video"
+                                                        data-toggle="modal" data-target="#modal1">
+                                                </div>
+
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <!--Modal: Name-->
+
+                                <a><img class="img-fluid z-depth-1"
+                                        src="https://mdbootstrap.com/img/screens/yt/screen-video-1.jpg"
+                                        alt="video"
+                                        data-toggle="modal" data-target="#modal1"></a>
+
+                            </div>
+
+                            <!--Imagem 2-->
+                            <div class="col-lg-4 col-md-12 mb-4">
+
+                                <!--Modal: Name-->
+                                <div class="modal fade" id="modal1" tabindex="-1" role="dialog"
+                                    aria-labelledby="myModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-lg" role="document">
+
+                                        <!--Content-->
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <p> asdhfgntjl,dewkmrfnjdm </p>
+                                                <button type="button" class="btn btn-outline-primary
+                                                btn-rounded btn-md ml-4" data-dismiss="modal"
+                                                aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                            </div>
+                                            <!--Body-->
+                                            <div class="modal-body mb-0 p-0">
+
+                                                <div class="embed-responsive">
+                                                    <img class="img-fluid z-depth-1"
+                                                        src="https://mdbootstrap.com/img/screens/yt/screen-video-1.jpg"
+                                                        alt="video"
+                                                        data-toggle="modal" data-target="#modal1">
+                                                </div>
+
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <!--Modal: Name-->
+
+                                <a><img class="img-fluid z-depth-1"
+                                        src="https://mdbootstrap.com/img/screens/yt/screen-video-1.jpg"
+                                        alt="video"
+                                        data-toggle="modal" data-target="#modal1"></a>
+
+                            </div>
+                             <!--Imagem 2-->
+                             <div class="col-lg-4 col-md-12 mb-4">
+
+                                    <!--Modal: Name-->
+                                    <div class="modal fade" id="modal1" tabindex="-1" role="dialog"
+                                        aria-labelledby="myModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-lg" role="document">
+
+                                            <!--Content-->
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <p> asdhfgntjl,dewkmrfnjdm </p>
+                                                    <button type="button" class="btn btn-outline-primary
+                                                    btn-rounded btn-md ml-4" data-dismiss="modal"
+                                                    aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                                </div>
+                                                <!--Body-->
+                                                <div class="modal-body mb-0 p-0">
+
+                                                    <div class="embed-responsive">
+                                                        <img class="img-fluid z-depth-1"
+                                                            src="https://mdbootstrap.com/img/screens/yt/screen-video-1.jpg"
+                                                            alt="video"
+                                                            data-toggle="modal" data-target="#modal1">
+                                                    </div>
+
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                    <!--Modal: Name-->
+
+                                    <a><img class="img-fluid z-depth-1"
+                                            src="https://mdbootstrap.com/img/screens/yt/screen-video-1.jpg"
+                                            alt="video"
+                                            data-toggle="modal" data-target="#modal1"></a>
+
+                                </div>
+                            </div>
+
+                        </div>
+
+
+                <div class="card-footer text-muted">
+                    30 agosto
+                </div>
             </div>
         </div>
     </div>
 </div>
-</div>
+
+
+
+
+
+
+
 
 
 @endsection
