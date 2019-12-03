c5edb8b2d2738d8e9730c0b3597a4733bbd1e0c4
marianunes17 <mariajoao.99@hotmail.com>
2019-12-03 18:05:39 +0000

    Update add-edit.blade.php

--- resources/views/eventos/partials/add-edit.blade.php
+++ resources/views/eventos/partials/add-edit.blade.php
@@ -1,10 +1,37 @@
 <div class="form-group">
-    <label for="inputName">Nome</label>
-    <input type="text" class="form-control" name="name" id="inputName"
-    value="{{old('name',$eventos->name)}}" />
+    <label for="inputTitulo">Titulo</label>
+    <input type="text" class="form-control" name="Titulo" id="inputTitulo"
+    value="{{old('Titulo',$eventos->Titulo)}}" />
     <!--Old - Se der erro vai mostrar o que estava no antigo-->
 </div>
+
+
 <div class="form-group">
-    <label for="inputDescription">Descrição</label>
-    <textarea class="form-control" name="description" id="inputDescription">{{old('description',$eventos->description)}}</textarea>
+    <label for="inputDescricao">Descrição</label>
+    <textarea class="form-control" name="descricao" id="inputDescricao">{{old('descricao',$eventos->descricao)}}</textarea>
+</div>
+
+<div class="form-group">
+    <label for="inputData">Data e hora</label>
+    <input type="datetime_local" class="form-control" name="data" id="inputData"
+        value="{{old('date',$post->date??date("Y-m-d H:i:s"))}}"/>
+        <!--date??date("Y-m-d H:i:s")) - Coloca a data por defeito-->
+</div>
+
+
+<div class="form-group">
+    <label for="inputImagem">Imagem</label>
+    <input type="file" class="form-control-file" name="imagem" id="inputImagem"
+        aria-describedby="fileHelp" />
+    <small id="fileHelp" class="form-text text-muted">
+        A imagem não deve ter mais de 2MB </small>
+</div>
+
+
+<div class="form-group">
+    <label for="inputCategoria">Categoria</label>
+    <select name="categoria" id="inputCategoria" class="form-control" value="{{old('categoria',$eventos->categoria)}}">
+            <option value="Projetos"> Projetos </option>
+            <option value="Eventos"> Eventos </option>
+    </select>
 </div>
