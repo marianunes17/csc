fc829fe6d927784e1cf182a96d79ce9f09a3e234
marianunes17 <mariajoao.99@hotmail.com>
2019-12-03 18:05:22 +0000

    Update eventos.php

--- app/eventos.php
+++ app/eventos.php
@@ -5,8 +5,8 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class eventos extends Model {
-    protected $fillable = [ 'name', 'description' ];
-    
+    protected $fillable = [ 'nome', 'descricao', 'data', 'categoria'];
+
     public function posts(){
         return $this->hasMany("App\Post","eventos_id");
     }
