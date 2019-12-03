263c2e434c5e7a06e2b840504b167f69b6b4646f
marianunes17 <mariajoao.99@hotmail.com>
2019-12-03 22:48:37 +0000

    Update admin_nav.blade.php

--- resources/views/layout/partials/admin_nav.blade.php
+++ resources/views/layout/partials/admin_nav.blade.php
@@ -1,37 +1,41 @@
 <div id="sidebar-nav" class="sidebar">
-        <img src="img/logos/logo-simbolo2.png" alt="Centro Social de
-        Carnide logo"> <span class="site-description">Centro Social de
-            Carnide</span>
+    <img src="{{asset('img/logos/logo-simbolo2.png')}}" alt="Centro Social de
+        Carnide logo" class="w-25"> <span class="site-description">Centro Social
+        de
+        Carnide</span>
     <div class="sidebar-scroll">
         <nav>
             <ul class="nav">
                 <li>
-                    <a href="#"> Estatisticas </a>
+                    <a href="{{asset('#')}}">
+                        <span>CSC</span>
+                    </a>
                 </li>
 
-
-                <li><a href="#"> Utilizadores</a>
+                <li><a href="#"><span> Utilizadores</span></a>
                     <ul class="nav">
                         <li>
-                            <a href="#">Adicionar Utilizador </a>
+                            <a href="#"><span>Adicionar Utilizador</span> </a>
                         </li>
                     </ul>
                 </li>
-
-
-
-                <li><a href="#""> Cotas </li>
 
                 <li>
-                    <a href="{{route('eventos.index')}}"> Eventos </a>
+                    <a href="#"> <span>Cotas </span></a>
+                </li>
+
+                <li>
+                    <a href="{{route('eventos.index')}}"><span> Eventos</span>
                     </a>
+
                     <ul class="nav">
                         <li>
-                            <a href="{{route('eventos.create')}}">Adicionar
-                                Evento</a>
+                            <a href="{{route('eventos.create')}}"><span>Adicionar
+                                    Evento</span></a>
                         </li>
                     </ul>
                 </li>
+
             </ul>
         </nav>
     </div>
