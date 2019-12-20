f65dcbbfe5a003dcb19c41c650a21677c7141c3a
CarolinaLopes99 <2180479@my.ipleiria.pt>
2019-12-20 15:18:47 +0000

    summary all

--- app/Http/Controllers/ParceriaController.php
+++ app/Http/Controllers/ParceriaController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Parceria;
 use Illuminate\Http\Request;
 
-class PartnershipController extends Controller
+class ParceriaController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -40,10 +41,10 @@ class PartnershipController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Parceria  $parceria
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Parceria $parceria)
     {
         //
     }
@@ -51,10 +52,10 @@ class PartnershipController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Parceria  $parceria
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Parceria $parceria)
     {
         //
     }
@@ -63,10 +64,10 @@ class PartnershipController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Parceria  $parceria
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Parceria $parceria)
     {
         //
     }
@@ -74,10 +75,10 @@ class PartnershipController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Parceria  $parceria
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Parceria $parceria)
     {
         //
     }
--- resources/views/parcerias/partials/add-edit.blade.php
+++ resources/views/parcerias/partials/add-edit.blade.php
@@ -1,13 +1,13 @@
 <div class="form-group">
     <label for="inputNome">Título</label>
     <input type="text" class="form-control" name="nome" id="inputNome"
-        value="{{old('nome',$evento->nome)}}" />
+        value="{{old('titulo',$parceria->titulo)}}" />
 </div>
 
 <div class="form-group">
     <label for="inputDescricao">Descrição</label>
     <textarea class="form-control" name="descricao"
-        id="inputDescricao">{{old('descricao',$evento->descricao)}}</textarea>
+        id="inputDescricao">{{old('descricao',$parceria->descricao)}}</textarea>
 </div>
 
 <div class="form-group">
@@ -17,20 +17,4 @@
     <small id="fileHelp" class="form-text text-muted">
         Please upload a valid file imagem. Size of imagem should not be more than
         2MB </small>
-</div>
-
-<div class="form-group">
-    <label for="inputCategory">Categoria</label>
-    <select name="category" id="inputCategory" class="form-control">
-        @foreach ($categories as $category)
-        <option {{ old('$category', $evento->category_id) == $category->id ? "selected":""}} value="{{$category->id}}" >{{$category->name}}</option>
-        @endforeach
-    </select>
-</div>
-
-
-<div class="form-group">
-    <label for="inputDate">Data</label>
-    <input type="date" class="form-control" name="data" id="inputDate"
-        value="{{old('data',$evento->data??date('Y-m-d'))}}"/>
 </div>
