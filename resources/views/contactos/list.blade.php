16e5ddb05ea72d10e9cc461d70c0814a9de6e06c
Tutag24 <58345778+Tutag24@users.noreply.github.com>
2020-01-16 15:47:06 +0000

    summary all

--- resources/views/contactos/list.blade.php
+++ resources/views/contactos/list.blade.php
@@ -29,28 +29,28 @@
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach($contactos as $contactos)
+                        @foreach($contactos as $contacto)
                         <tr>
-                            <td>{{$contactos->nome}}</td>
-                            <td>{{$contactos->email}}</td>
-                            <td>{{$contactos->assunto}}</td>
-                            <td>{{$contactos->mensagem}}</td>
-                            <td>{{$contactos->data}}</td>
+                            <td>{{$contacto->nome}}</td>
+                            <td>{{$contacto->email}}</td>
+                            <td>{{$contacto->assunto}}</td>
+                            <td>{{$contacto->mensagem}}</td>
+                            <td>{{$contacto->data}}</td>
 
 
                             </td>
                             <td nowrap>
-                                <a href="{{route('contactos.show',$contactos)}}"
+                                <a href="{{route('contactos.show',$contacto)}}"
                                     class="btn btn-sm btn-link text-decoration-none d-inline" title="Visualizar Contacto">
                                     <i class="fas fa-eye fa-xs text-primary" style="font-size: 15px;"></i>
                                 </a>
 
-                                <a href="mailto:{{$contactos->Email}}"
+                                <a href="mailto:{{$contacto->Email}}"
                                     class="btn btn-sm btn-link d-inline text-decoration-none" title="Enviar Mensagem">
                                     <i class="fas fa-envelope fa-xs text-warning" style="font-size: 15px;"></i>
                                 </a>
 
-                                <form method="POST" action="{{route('contactos.destroy',$contactos)}}" role="form"
+                                <form method="POST" action="{{route('contactos.destroy',$contacto)}}" role="form"
                                     class="d-inline"
                                     onsubmit="return confirm('Tem a certeza que quer eliminar o contacto?');">
                                     @csrf
