2f16e4f62033d5d5039390beb29772043cbc5aa1
marianunes17 <mariajoao.99@hotmail.com>
2020-01-31 02:51:52 +0000

    summary all

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -73,15 +73,14 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Tem a certeza que quer sair?</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Tem a certeza que deseja terminar sessão?</h5>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">×</span>
                     </button>
                 </div>
-                <div class="modal-body">Clique em "Sair" se tiver a certeza.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
-                    <a class="btn btn-primary" href="login.html">Sair</a>
+                    <a class="btn btn-primary" href="{{route('csc.index')}}">Sair</a>
                 </div>
             </div>
         </div>
