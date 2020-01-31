7774191134974e1a83d6b9e7e0f583df155fb92f
marianunes17 <mariajoao.99@hotmail.com>
2020-01-31 16:23:47 +0000

    summary all

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -80,7 +80,7 @@
                 </div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
-                    <a class="btn btn-primary" href="{{route('csc.index')}}">Sair</a>
+                    <a class="btn btn-primary" href="{{route('dashboard')}}">Sair</a>
                 </div>
             </div>
         </div>
